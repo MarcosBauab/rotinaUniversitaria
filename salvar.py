4f1e80c90125a2c7d694651f8bf370dcf006687c
Marcos Vinicius Gasparoto Bauab <marcosbauab@gmail.com>
2021-09-01 20:40:15 -0300

    Arquivo adicional

---
+++ salvar.py
@@ -0,0 +1,3 @@
+periodo = 'noite'
+inventario = {'Dinheiro': 85, 'Itens': [['Bola - 20 reais']], 'Comidas': []}
+habilidades = {'alegria': 7, 'energia': 5, 'esportes': 7, 'amizades': 7, 'organizacao': 3, 'saude': 2}
